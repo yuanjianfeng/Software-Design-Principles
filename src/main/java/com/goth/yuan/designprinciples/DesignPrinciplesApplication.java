b61a83095558d6c5dc22ebb67d42294159145efe
jianfeng.yuan <goth.yuan@yatsengglobal.com>
2022-03-31 10:53:59 +0800

    初始化

---
+++ src/main/java/com/goth/yuan/designprinciples/DesignPrinciplesApplication.java
@@ -0,0 +1,13 @@
+package com.goth.yuan.designprinciples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPrinciplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPrinciplesApplication.class, args);
+    }
+
+}
